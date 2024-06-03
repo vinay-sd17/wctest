3b519870063a34a5cc210be8d4688a78eec31b68
Desikavinayagam S <60941690+vinay-sd17@users.noreply.github.com>
2024-06-03 11:42:20 +0530

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -18,3 +18,28 @@ private HashMap<String, Product> getProductsMapAsync(List<Long> ids) {
         executor.shutdown();
         return map;
     }
+
+
+
+private List<Product> getProductsFromService(List<Long> ids) {
+        List<CompletableFuture<Product>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        AtomicReference<List<Product>> products = new AtomicReference<>(new ArrayList<>());
+        ids.forEach(id -> {
+            CompletableFuture<CompletableFuture<Product>> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return productService.getProduct(id);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }, executor);
+            futures.add(future.thenCompose(f -> f));
+        });
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allFutures.thenAccept(v -> {
+            products.set(futures.stream().map(CompletableFuture::join).collect(Collectors.toList()));
+        }).join();
+        return products.get();
+    }
